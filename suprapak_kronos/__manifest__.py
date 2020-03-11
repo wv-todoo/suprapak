1992c867f910d38740f94429272b745717f04cf9
LIvingston <la@todoo.co>
2020-03-11 08:38:17 -0500

    desarrollo kronos

--- suprapak_kronos/__manifest__.py
+++ suprapak_kronos/__manifest__.py
@@ -21,7 +21,7 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/data_sheet_view.xml',
