859c0b0abccc6469e655ab4af2d1fb43eaee8662
LIvingston <la@todoo.co>
2020-03-13 15:06:33 -0500

    17934 [ADD] cambios en el modylo kronos para probar rutas y lista de producción

--- suprapak_kronos/__manifest__.py
+++ suprapak_kronos/__manifest__.py
@@ -17,14 +17,15 @@
     'version': '13.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale_crm'],
+    'depends': ['sale_crm','mrp'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/data_sheet_view.xml',
+        'views/data_sheet_menu.xml',
         'views/crm_lead_view.xml',
         'views/sale_order_view.xml',
     ],
--- suprapak_kronos/models/data_sheet.py
+++ suprapak_kronos/models/data_sheet.py
@@ -31,13 +31,14 @@ class DataSheet(models.Model):
     print_class = fields.Selection([('external','External'),('internal','Internal')],'Print Class')
     print_type = fields.Many2one('print.type','Print Type')
     uom_id = fields.Many2one('uom.uom', 'Unit of measure')
-    product_type_id = fields.Many2one('data.product.type', 'Product type')
+    product_type_id = fields.Many2one('data.product.type', 'Product line')
     draw_type_id = fields.Many2one('data.draw.type', 'Draw type')
     movie_type_id = fields.Many2one('data.movie.type', 'Movie type')
     color_movie__id = fields.Many2one('data.movie.color', 'Color movie')
+    chemical_composition = fields.Many2one('chemical.composition','Chemical Composition')
     # Info cant
-    specification_width = fields.Integer('Specification width')
-    specification_long = fields.Integer('Specification long')
+    specification_width_id = fields.Many2one('specification.width','Specification width')
+    specification_long_id = fields.Many2one('specification.long','Specification long')
     caliber_id = fields.Many2one('data.caliber.type', 'Specification caliber')
     tolerance_width = fields.Integer('Tolerance width')
     tolerance_long = fields.Integer('Tolerance long')
@@ -46,7 +47,10 @@ class DataSheet(models.Model):
     tongue = fields.Boolean('Tongue')
     thermal_adhesive = fields.Boolean('Thermal adhesive')
     print = fields.Boolean('Print')
-    no_print = fields.Boolean('without print')
+    no_print = fields.Boolean('Without Print')
+    rhombus = fields.Boolean('Rhombus')
+    guillotine = fields.Boolean('Requires Guillotine')
+    guillotine = fields.Boolean('')
 
     # Comments
     comments = fields.Text('Comments')
@@ -54,11 +58,25 @@ class DataSheet(models.Model):
     quotation_count = fields.Integer(compute='_compute_sale_data', string="Number of Quotations")
     order_ids = fields.One2many('sale.order', 'sheet_id', string='Orders')
     photo = fields.Binary()
-    form_id = fields.Many2one('data.form','Form')
+    tag_form_id = fields.Many2one('data.tag.form','Tag Form')
     material_id = fields.Many2one('data.material','Material')
     application_id = fields.Many2one('data.application.mode','Application Mode')
     position_id = fields.Many2one('data.application.position','Application Position')
     content_id = fields.Many2one('data.application.contents','Package Contents')
+    quantity = fields.Char('Quantity')
+    form_id = fields.Many2one('data.form','Form')
+    overlap_id = fields.Many2one('Width.overlap','Width Overlap')
+    tolerance = fields.Char('Tolerance')
+    overlap_location_id = fields.Many2one('overlap.location','Overlap Location')
+    list_id = fields.Many2one('mrp.bom')
+    lists_ids = fields.Many2many('mrp.bom','sheet_bom_rel','sheet_id','bom_id','Bills of Materials')
+    routing_id = fields.Many2one('mrp.routing','Routings')
+    routings_ids = fields.Many2many('mrp.routing','sheet_routing_rel','sheet_id','routing_id','Routing')
+
+    @api.onchange('overlap_id')
+    def _onchange_overlap_id(self):
+        if self.overlap_id:
+            self.tolerance = self.overlap_id.tolerance
 
 
     def _compute_sale_data(self):
@@ -96,8 +114,8 @@ class DataSheet(models.Model):
                 'product_type_id': record.product_type_id.id,
                 'draw_type_id': record.draw_type_id.id,
                 'movie_type_id': record.movie_type_id.id,
-                'specification_width': record.specification_width,
-                'specification_long': record.specification_long,
+                'specification_width': record.specification_width_id.id,
+                'specification_long': record.specification_long_id.id,
                 'caliber_id': record.caliber_id.id,
                 'tongue': record.tongue,
                 'thermal_adhesive': record.thermal_adhesive,
@@ -128,8 +146,8 @@ class DataSheet(models.Model):
             'product_type_id': self.product_type_id.id,
             'draw_type_id': self.draw_type_id.id,
             'movie_type_id': self.movie_type_id.id,
-            'specification_width': self.specification_width,
-            'specification_long': self.specification_long,
+            'specification_width': self.specification_width_id.id,
+            'specification_long': self.specification_long_id.id,
             'caliber_id': self.caliber_id.id,
         }
         action['context'] = {
@@ -207,6 +225,13 @@ class DataForm(models.Model):
     name = fields.Char('Form')
     code = fields.Char('code')
 
+class DataTagForm(models.Model):
+    _name = 'data.tag.form'
+    _description = 'Tag Form'
+
+    name = fields.Char('Tag Form')
+    code = fields.Char('code')
+
 class DataMaterial(models.Model):
     _name = 'data.material'
     _description = 'Material'
@@ -229,7 +254,7 @@ class DataAplication(models.Model):
     code = fields.Char('code')
 
 class DataContents(models.Model):
-    _name = 'data.package.contents'
+    _name = 'data.application.contents'
     _description = 'Package Contents'
 
     name = fields.Char('Package Contents')
@@ -242,3 +267,39 @@ class PrintType(models.Model):
     name = fields.Char('Print Type')
     code = fields.Char('code')
 
+
+class ChemicalComposition(models.Model):
+    _name = 'chemical.composition'
+    _description = 'Chemical Composition'
+
+    name = fields.Char('Chemical Composition')
+    code = fields.Char('code')
+
+class SpecificationWidth(models.Model):
+    _name = 'specification.width'
+    _description = 'Specification Width'
+
+    name = fields.Char('Specification Width')
+    code = fields.Char('code')
+
+class SpecificationLong(models.Model):
+    _name = 'specification.long'
+    _description = 'Specification Long'
+
+    name = fields.Char('Specification Long')
+    code = fields.Char('code')
+
+class Widthoverlap(models.Model):
+    _name = 'width.overlap'
+    _description = 'Width Overlap'
+
+    name = fields.Char('Width Overlap')
+    tolerance = fields.Char('Tolerance')
+    code = fields.Char('code')
+
+class Widthoverlap(models.Model):
+    _name = 'overlap.location'
+    _description = 'Overlap Location'
+
+    name = fields.Char('Overlap Location')
+    code = fields.Char('code')
